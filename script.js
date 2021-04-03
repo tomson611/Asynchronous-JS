d37b2cbe27341b649dc5bbfadea6ac7e89eb6562
Tomson <twesolowski61@gmail.com>
2021-04-03 13:42:42 +0200

    chaining promises

--- script.js
+++ script.js
@@ -67,6 +67,14 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function (country) {
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[1];
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
-getCountryData('portugal');
+getCountryData('poland');
