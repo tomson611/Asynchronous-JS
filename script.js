e371687521710c0f30552769d6945017ea963b92
Tomson <twesolowski61@gmail.com>
2021-04-06 18:39:30 +0200

    running promises in parallel

--- script.js
+++ script.js
@@ -297,7 +297,25 @@ const wait = function (seconds) {
 
 const get3Countries = async function (c1, c2, c3) {
   try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
   } catch (err) {
     console.error(err);
   }
 };
+get3Countries('poland', 'portugal', 'germany');
